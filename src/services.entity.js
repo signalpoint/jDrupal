09444bcc2f2a782cb1d8d7a3ed2e9e770692654b
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-01 15:53:06 -0500

    Improved entity index to support query objects, sweet.

--- src/services.entity.js
+++ src/services.entity.js
@@ -53,14 +53,12 @@ function entity_retrieve(entity_type, ids, options) {
  */
 function entity_update(entity_type, bundle, entity, options) {
   try {
-    // Wrap entities, except for vocabularies.
+    // Wrap entities, except for taxonomy.
     var entity_wrapper = {};
-    if (entity_type == 'taxonomy_vocabulary') {
+    if (entity_type == 'taxonomy_term' || entity_type == 'taxonomy_vocabulary') {
       entity_wrapper = entity;
     }
-    else {
-      entity_wrapper[entity_type] = entity;
-    }
+    else { entity_wrapper[entity_type] = entity; }
     Drupal.services.call({
         method: options.method,
         path: options.path,
@@ -85,10 +83,20 @@ function entity_update(entity_type, bundle, entity, options) {
  */
 function entity_index(entity_type, query, options) {
   try {
+    var query_string;
+    if (typeof query === 'object') {
+      query_string = entity_index_build_query_string(query);
+    }
+    else if (typeof query === 'string') {
+      query_string = query;
+    }
+    if (query_string) { query_string = '&' + query_string; }
+    else { query_string = ''; }
     Drupal.services.call({
         method: 'GET',
-        path: entity_type + '.json',
-        data: JSON.stringify(query),
+        path: entity_type + '.json' + query_string,
+        /*data: JSON.stringify(query),*/
+        /*data:query,*/
         success: function(result) {
           if (options.success) { options.success(result); }
         },
@@ -98,5 +106,47 @@ function entity_index(entity_type, query, options) {
     });
   }
   catch (error) { console.log('entity_index - ' + error); }
+}
+/**
+ * Builds a query string from a query object for an entity index resource.
+ * @param {Object} query
+ * @return {String}
+ */
+function entity_index_build_query_string(query) {
+  try {
+    var result = '';
+    if (query.fields) { // array
+      var fields = '';
+      for (var i = 0; i < query.fields.length; i++) {
+        fields += encodeURIComponent(query.fields[i]) + ',';
+      }
+      if (fields != '') {
+        fields = 'fields=' + fields.substring(0, fields.length - 1);
+        result += fields + '&';
+      }
+    }
+    if (query.parameters) { // object
+      var parameters = '';
+      for (var parameter in query.parameters) {
+          if (query.parameters.hasOwnProperty(parameter)) {
+            var key = encodeURIComponent(parameter);
+            var value = encodeURIComponent(query.parameters[parameter]);
+            parameters += 'parameters[' + key + ']=' + value + '&';
+          }
+      }
+      if (parameters != '') {
+        parameters = parameters.substring(0, parameters.length - 1);
+        result += parameters + '&';
+      }
+    }
+    if (typeof query.page !== 'undefined') { // int
+      result += 'page=' + encodeURIComponent(query.page) + '&';
+    }
+    if (typeof query.page_size !== 'undefined') { // int
+      result += 'page_size=' + encodeURIComponent(query.page_size) + '&';
+    }
+    return result.substring(0, result.length - 1);
+  }
+  catch (error) { console.log('entity_index_build_query_string - ' + error); }
 }
 
