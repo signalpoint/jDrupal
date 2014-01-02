d5152983797583a7aae14b5857b7999b52470796
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-02 14:43:00 -0500

    Changed system connect to automatically grab a token before connecting if one doesn't exist. Added a reset option.

--- src/services.system.js
+++ src/services.system.js
@@ -4,17 +4,53 @@
  */
 function system_connect(options) {
   try {
-    Drupal.services.call({
-        method: 'POST',
-        path: 'system/connect.json',
-        success: function(data) {
-          Drupal.user = data.user;
-          if (options.success) { options.success(data); }
-        },
-        error: function(xhr, status, message) {
-          if (options.error) { options.error(xhr, status, message); }
-        }
-    });
+    
+    // Build a system connect object.
+    var system_connect = {
+      method: 'POST',
+      path: 'system/connect.json',
+      success: function(data) {
+        Drupal.user = data.user;
+        if (options.success) { options.success(data); }
+      },
+      error: function(xhr, status, message) {
+        if (options.error) { options.error(xhr, status, message); }
+      }
+    };
+    
+    // If we don't have a token, grab one first.
+    if (!Drupal.csrf_token) {
+      services_get_csrf_token({
+          success: function(token) {
+            try {
+              if (options.debug) { console.log('Grabbed new token.'); }
+              // Now that we have a token, make the system connect call.
+              Drupal.csrf_token = true;
+              Drupal.services.call(system_connect);
+            }
+            catch (error) {
+              console.log(
+                'system_connect - services_csrf_token - success - ' + message
+              );
+            }
+          },
+          error: function(xhr, status, message) {
+            try {
+              if (options.error) { options.error(xhr, status, message); }
+            }
+            catch (error) {
+              console.log(
+                'system_connect - services_csrf_token - error - ' + message
+              );
+            }
+          }
+      });
+    }
+    else {
+      // We already have a token, make the system connect call.
+      if (options.debug) { console.log('Token already available.'); }
+      Drupal.services.call(system_connect);
+    }
   }
   catch (error) {
     console.log('system_connect - ' + error);
