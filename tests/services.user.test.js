f4f1cca182ce17501d10f19012eecb029cc0694e
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-02 14:30:42 -0500

    Added a fail login test.

--- tests/services.user.test.js
+++ tests/services.user.test.js
@@ -23,6 +23,8 @@ function test_services_user_new_template() {
 
 var test_user_crud = function(name, pass, callback) {
   
+  // TODO - need a failed user login test
+  
   // User Logout
   asyncTest("user_logout", function() {
       user_logout({
@@ -31,126 +33,138 @@ var test_user_crud = function(name, pass, callback) {
             expect(1);
             ok(result[0], "lgged out");
             
-            // User Register
-            asyncTest("user_register", function() {
-                user_register(test_services_user_new_template(), {
-                    success:function(result){
+            // Login attempt fail
+            asyncTest("user_login - bad credentials", function() {
+                user_login(user_password(), user_password(), {
+                    error:function(xhr, status, message){
                       start();
                       expect(1);
-                      ok(result.uid, "uid");
-                      var new_uid = result.uid;
+                      ok(message[0], "Wrong username or password.");
                       
-                      // Log back in...
-                      asyncTest("user_login - logging back in", function() {
-                          user_login(name, pass, {
+                      // User Register
+                      asyncTest("user_register", function() {
+                          user_register(test_services_user_new_template(), {
                               success:function(result){
                                 start();
                                 expect(1);
-                                ok(result.user.name == name, "name");
+                                ok(result.uid, "uid");
+                                var new_uid = result.uid;
                                 
-                                // Delete newly registered user...
-                                asyncTest("user_delete - deleting newly registered user", function() {
-                                    user_delete(new_uid, {
-                                        success:function(result) {
+                                // Log back in...
+                                asyncTest("user_login - logging back in", function() {
+                                    user_login(name, pass, {
+                                        success:function(result){
                                           start();
                                           expect(1);
-                                          ok(result[0], "deleted");
+                                          ok(result.user.name == name, "name");
                                           
-                                          // Create
-                                          asyncTest("user_save - create new", function() {
-                                              var user = test_services_user_new_template();
-                                              user_save(user, {
-                                                  success:function(user_create_result){
+                                          // Delete newly registered user...
+                                          asyncTest("user_delete - deleting newly registered user", function() {
+                                              user_delete(new_uid, {
+                                                  success:function(result) {
                                                     start();
-                                                    expect(2);
-                                                    ok(!!user_create_result.uid, "uid");
-                                                    ok(!!user_create_result.uri, "uri");
+                                                    expect(1);
+                                                    ok(result[0], "deleted");
                                                     
-                                                    // Retrieve
-                                                    asyncTest("user_load", function() {
-                                                        user_load(user_create_result.uid, {
-                                                            success:function(user_retrieve_result) {
+                                                    // Create
+                                                    asyncTest("user_save - create new", function() {
+                                                        var user = test_services_user_new_template();
+                                                        user_save(user, {
+                                                            success:function(user_create_result){
                                                               start();
                                                               expect(2);
-                                                              ok(user_retrieve_result.uid == user_create_result.uid, "uid");
-                                                              ok(user_retrieve_result.title == user.title, "title");
+                                                              ok(!!user_create_result.uid, "uid");
+                                                              ok(!!user_create_result.uri, "uri");
                                                               
-                                                              // Update
-                                                              asyncTest("user_save - update existing", function() {
-                                                                  var user_changes = {
-                                                                    uid:user_retrieve_result.uid,
-                                                                    mail:user_password() + '@example.com',
-                                                                    current_pass: user.pass
-                                                                  };
-                                                                  user_save(user_changes, {
-                                                                      success:function(user_update_result) {
+                                                              // Retrieve
+                                                              asyncTest("user_load", function() {
+                                                                  user_load(user_create_result.uid, {
+                                                                      success:function(user_retrieve_result) {
                                                                         start();
-                                                                        expect(1);
-                                                                        ok(user_update_result.uid == user_create_result.uid, "uid");
+                                                                        expect(2);
+                                                                        ok(user_retrieve_result.uid == user_create_result.uid, "uid");
+                                                                        ok(user_retrieve_result.title == user.title, "title");
                                                                         
-                                                                        // Retrieve updated entity.
-                                                                        asyncTest("user_load - after update", function() {
-                                                                            user_load(user_update_result.uid, {
-                                                                                success:function(updated_user){
+                                                                        // Update
+                                                                        asyncTest("user_save - update existing", function() {
+                                                                            var user_changes = {
+                                                                              uid:user_retrieve_result.uid,
+                                                                              mail:user_password() + '@example.com',
+                                                                              current_pass: user.pass
+                                                                            };
+                                                                            user_save(user_changes, {
+                                                                                success:function(user_update_result) {
                                                                                   start();
-                                                                                  expect(2);
-                                                                                  ok(user_update_result.uid == updated_user.uid, "uid");
-                                                                                  ok(user_update_result.mail == updated_user.mail, 'mail (' + user_update_result.mail + ' ?= ' + updated_user.mail + ')');
+                                                                                  expect(1);
+                                                                                  ok(user_update_result.uid == user_create_result.uid, "uid");
                                                                                   
-                                                                                  // Delete
-                                                                                  asyncTest("user_delete", function() {
-                                                                                      user_delete(updated_user.uid, {
-                                                                                          success:function(user_delete_result){
+                                                                                  // Retrieve updated entity.
+                                                                                  asyncTest("user_load - after update", function() {
+                                                                                      user_load(user_update_result.uid, {
+                                                                                          success:function(updated_user){
                                                                                             start();
-                                                                                            expect(1);
-                                                                                            ok(user_delete_result[0], "deleted");
+                                                                                            expect(2);
+                                                                                            ok(user_update_result.uid == updated_user.uid, "uid");
+                                                                                            ok(user_update_result.mail == updated_user.mail, 'mail (' + user_update_result.mail + ' ?= ' + updated_user.mail + ')');
                                                                                             
-                                                                                            // Index
-                                                                                            asyncTest("user_index", function() {
-                                                                                                var query = {
-                                                                                                  type: 'article'
-                                                                                                };
-                                                                                                user_index(query, {
-                                                                                                    success:function(user_index_results){
+                                                                                            // Delete
+                                                                                            asyncTest("user_delete", function() {
+                                                                                                user_delete(updated_user.uid, {
+                                                                                                    success:function(user_delete_result){
                                                                                                       start();
                                                                                                       expect(1);
-                                                                                                      ok(user_index_results[0].uid, "uid");
+                                                                                                      ok(user_delete_result[0], "deleted");
                                                                                                       
-                                                                                                      if (callback) {
-                                                                                                        test_services_node(callback);
-                                                                                                        //callback();
-                                                                                                      }
+                                                                                                      // Index
+                                                                                                      asyncTest("user_index", function() {
+                                                                                                          var query = {
+                                                                                                            type: 'article'
+                                                                                                          };
+                                                                                                          user_index(query, {
+                                                                                                              success:function(user_index_results){
+                                                                                                                start();
+                                                                                                                expect(1);
+                                                                                                                ok(user_index_results[0].uid, "uid");
+                                                                                                                
+                                                                                                                if (callback) {
+                                                                                                                  test_services_node(callback);
+                                                                                                                  //callback();
+                                                                                                                }
+                                                                                                              }
+                                                                                                          });
+                                                                                                      }); // Index
+                                                                                                      
                                                                                                     }
                                                                                                 });
-                                                                                            }); // Index
+                                                                                            }); // Delete
                                                                                             
                                                                                           }
                                                                                       });
-                                                                                  }); // Delete
+                                                                                  }); // Update Existing
                                                                                   
                                                                                 }
                                                                             });
-                                                                        }); // Update Existing
+                                                                        }); // Load After Create
                                                                         
                                                                       }
                                                                   });
-                                                              }); // Load After Create
+                                                              }); // Retrieve
                                                               
                                                             }
                                                         });
-                                                    }); // Retrieve
+                                                    }); // Create
                                                     
                                                   }
                                               });
-                                          }); // Create
+                                          });
                                           
                                         }
                                     });
-                                });
+                                });                     
                                 
                               }
                           });
-                      });                     
+                      });
                       
                     }
                 });
