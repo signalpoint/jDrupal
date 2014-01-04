287aa893e9fadb411db99e6acd7151dc46c8a159
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-04 16:35:52 -0500

    Added file_load().

--- src/file.js
+++ src/file.js
@@ -1,1 +1,12 @@
+/**
+ * Loads a file, given a file id.
+ * @param {Number} fid
+ * @param {Object} options
+ */
+function file_load(fid, options) {
+  try {
+    entity_load('file', fid, options);
+  }
+  catch (error) { console.log('file_load - ' + error); }
+}
 
