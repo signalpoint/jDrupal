0ca677233dec269508b4483050d9fd8f3c1ddc81
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-08 00:04:48 -0500

    Removed the app directory, too heavy for a single file JS library, derp.

--- app/default.settings.js
+++
@@ -1,6 +0,0 @@
-// Set the site path (without the trailing slash).
-Drupal.settings.site_path = ""; // http://www.example.com
-
-// Set the Service Resource endpoint path.
-Drupal.settings.endpoint = "";
-
