affbce6936178eed2b88d5d34bb2d9d97cabccb0
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-11 00:26:36 -0500

    Added hook_services_request_pre_postprocess_alter().

--- src/API.js
+++ src/API.js
@@ -1,12 +1,30 @@
+
+
 /**
- * Alter the result data of a service call.
+ * Alter the result data of a service call, before its success function.
+ * @param {object} controller
+ * @param {Object} result
+ */
+function hook_services_request_pre_postprocess_alter(options, result) {
+  try {
+    if (options.service == 'user' && options.resource == 'login') {
+      result.user.extra_cool = true;
+    }
+  }
+  catch (error) {
+    console.log('hook_services_request_pre_postprocess_alter - ' + error);
+  }
+}
+
+/**
+ * Alter the result data of a service call, after its success function.
  * @param {object} controller
  * @param {Object} result
  */
 function hook_services_request_postprocess_alter(options, result) {
   try {
     if (options.service == 'user' && options.resource == 'login') {
-      result.user.extra_cool = true;
+      Drupal.user.extra_cool = false;
     }
   }
   catch (error) {
