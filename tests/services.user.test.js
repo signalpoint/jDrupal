cc58e5d9368d3543c80a37a61c6ba877989bacd2
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-01-09 21:57:44 -0600

    Update user tests for D8, skipped C.U.D. for now.

--- tests/services.user.test.js
+++ tests/services.user.test.js
@@ -34,63 +34,65 @@ var test_user_crud = function(name, pass, callback) {
             start();
             expect(1);
             ok(result, "logged out");
-
-            return;
             
             // Login attempt fail
             asyncTest("user_login - bad credentials", function() {
                 user_login(user_password(), user_password(), {
-                    error:function(xhr, status, message){
+                    error:function(xhr, status, response){
                       start();
                       expect(1);
-                      ok(message[0], "Wrong username or password.");
+                      ok(response.message, "Not Acceptable.");
                       
                       // User Register
-                      asyncTest("user_register", function() {
+                      /*asyncTest("user_register", function() {
                           user_register(test_services_user_new_template(), {
                               success:function(result){
                                 start();
                                 expect(1);
                                 ok(result.uid, "uid");
-                                var new_uid = result.uid;
+                                var new_uid = result.uid;*/
                                 
                                 // Log back in...
                                 asyncTest("user_login - logging back in", function() {
                                     user_login(name, pass, {
                                         success:function(result){
+                                          console.log(result);
                                           start();
                                           expect(1);
-                                          ok(result.user.name == name, "name");
+                                          ok(result.name[0].value == name, "name");
                                           
                                           // Delete newly registered user...
-                                          asyncTest("user_delete - deleting newly registered user", function() {
+                                          /*asyncTest("user_delete - deleting newly registered user", function() {
                                               user_delete(new_uid, {
                                                   success:function(result) {
                                                     start();
                                                     expect(1);
-                                                    ok(result[0], "deleted");
+                                                    ok(result[0], "deleted");*/
                                                     
                                                     // Create
-                                                    asyncTest("user_save - create new", function() {
+                                                    /*asyncTest("user_save - create new", function() {
                                                         var user = test_services_user_new_template();
                                                         user_save(user, {
                                                             success:function(user_create_result){
                                                               start();
                                                               expect(2);
                                                               ok(!!user_create_result.uid, "uid");
-                                                              ok(!!user_create_result.uri, "uri");
+                                                              ok(!!user_create_result.uri, "uri");*/
                                                               
                                                               // Retrieve
                                                               asyncTest("user_load", function() {
-                                                                  user_load(user_create_result.uid, {
+                                                                  //user_load(user_create_result.uid, {
+                                                                  user_load(result.uid[0]['value'], {
                                                                       success:function(user_retrieve_result) {
                                                                         start();
                                                                         expect(2);
-                                                                        ok(user_retrieve_result.uid == user_create_result.uid, "uid");
-                                                                        ok(user_retrieve_result.title == user.title, "title");
+                                                                        //ok(user_retrieve_result.uid == user_create_result.uid, "uid");
+                                                                        //ok(user_retrieve_result.title == user.title, "title");
+                                                                        ok(user_retrieve_result.uid[0].value == result.uid[0].value, "uid");
+                                                                        ok(user_retrieve_result.name[0].value == result.name[0].value, "name");
                                                                         
                                                                         // Update
-                                                                        asyncTest("user_save - update existing", function() {
+                                                                        /*asyncTest("user_save - update existing", function() {
                                                                             var user_changes = {
                                                                               uid:user_retrieve_result.uid,
                                                                               mail:user_password() + '@example.com',
@@ -129,11 +131,11 @@ var test_user_crud = function(name, pass, callback) {
                                                                                                                 start();
                                                                                                                 expect(1);
                                                                                                                 ok(user_index_results[0].uid, "uid");
-                                                                                                                
+                                                                                                                */
                                                                                                                 if (callback) {
                                                                                                                   test_services_node(callback);
                                                                                                                   //callback();
-                                                                                                                }
+                                                                                                                }/*
                                                                                                               }
                                                                                                           });
                                                                                                       }); // Index
@@ -144,31 +146,31 @@ var test_user_crud = function(name, pass, callback) {
                                                                                             
                                                                                           }
                                                                                       });
-                                                                                  }); // Update Existing
+                                                                                  }); // Retrieve Existing
                                                                                   
                                                                                 }
                                                                             });
-                                                                        }); // Load After Create
+                                                                        }); // Update After Create*/
                                                                         
                                                                       }
                                                                   });
                                                               }); // Retrieve
                                                               
-                                                            }
-                                                        });
-                                                    }); // Create
+                                                            //
+                                                        //});
+                                                    //}); // Create
                                                     
-                                                  }
-                                              });
-                                          });
+                                                  //}
+                                              //});
+                                          //}); // Delete newly registered user
                                           
                                         }
                                     });
                                 });                     
                                 
-                              }
-                          });
-                      });
+                              //}
+                          //});
+                      //}); // Register
                       
                     }
                 });
