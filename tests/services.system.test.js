60efe2ff2c20caad06c376cf52616669125256a3
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-01-11 10:40:35 -0600

    Update system connect test to use Drupal.currentUser().

--- tests/services.system.test.js
+++ tests/services.system.test.js
@@ -10,11 +10,11 @@ var test_connect = function(callback) {
   asyncTest("jdrupal_connect", function() {
       jdrupal_connect({
           success:function(result){
-            console.log(result);
             start();
             expect(2);
-            ok(!!result.account, "account");
-            ok(!!result.account.uid, "uid");
+            var account = Drupal.currentUser();
+            ok(!!account, "account");
+            ok(!!account.id(), "uid");
             if (callback) {
               callback();
             }
