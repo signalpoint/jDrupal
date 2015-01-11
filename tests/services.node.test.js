c43e0c2ba6a05b8e6dc9b1a93c69dd988544f5f1
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-01-11 10:40:06 -0600

    Update node tests to use Drupal.Entity.

--- tests/services.node.test.js
+++ tests/services.node.test.js
@@ -7,10 +7,10 @@ var test_services_node = function(callback) {
 };
 
 function test_services_node_template() {
-  return {
+  return new Drupal.Entity.Node({
     type: [ { target_id: 'page' } ],
     title: [ { value: user_password() } ]
-  };
+  });
 }
 
 var test_node_crud = function(callback) {
@@ -31,41 +31,42 @@ var test_node_crud = function(callback) {
                     success:function(node_retrieve_result) {
                       start();
                       expect(2);
-                      ok(node_retrieve_result.nid[0].value == node_create_result_nid, "nid");
-                      ok(node_retrieve_result.title[0].value == node.title[0].value, "title");
+                      ok(node_retrieve_result.id() == node_create_result_nid, "nid");
+                      ok(node_retrieve_result.getTitle() == node.getTitle(), "title");
                       
                       // Update
                       asyncTest("node_save - update existing", function() {
-                          var node_changes = {
-                            nid:node_retrieve_result.nid,
-                            title:user_password()
-                          };
+                          var node_changes = new Drupal.Entity.Node({
+                              nid: [{ value: node_create_result_nid }],
+                              type: [{ target_id: node_retrieve_result.getType() }],
+                              title: [{ value: user_password() }]
+                          });
                           node_save(node_changes, {
-                              success:function(node_update_result) {
+                              success:function() {
                                 start();
                                 expect(1);
-                                ok(node_update_result.nid == node_create_result.nid, "nid");
+                                ok(arguments.length == 0, "204 - No Content");
                                 
                                 // Retrieve updated entity.
                                 asyncTest("node_load - after update", function() {
-                                    node_load(node_update_result.nid, {
+                                    node_load(node_changes.id(), {
                                         success:function(updated_node){
                                           start();
                                           expect(3);
-                                          ok(node_update_result.nid == updated_node.nid, "nid");
-                                          ok(node_changes.title == updated_node.title, "title");
-                                          ok(node.title != updated_node.title, "title changed (" + node.title + " != " + updated_node.title + ")");
+                                          ok(node_changes.id() == updated_node.id(), "nid");
+                                          ok(node_changes.getTitle() == updated_node.getTitle(), "title");
+                                          ok(node.getTitle() != updated_node.getTitle(), "title changed (" + node.getTitle() + " != " + updated_node.getTitle() + ")");
                                           
                                           // Delete
                                           asyncTest("node_delete", function() {
-                                              node_delete(updated_node.nid, {
-                                                  success:function(node_delete_result){
+                                              node_delete(updated_node.id(), {
+                                                  success:function(){
                                                     start();
                                                     expect(1);
-                                                    ok(node_delete_result[0], "deleted");
+                                                    ok(arguments.length == 0, "204 - No Content");
                                                     
                                                     // Index
-                                                    asyncTest("node_index", function() {
+                                                    /*asyncTest("node_index", function() {
                                                         var query = {
                                                           type: 'article'
                                                         };
@@ -73,15 +74,15 @@ var test_node_crud = function(callback) {
                                                             success:function(node_index_results){
                                                               start();
                                                               expect(1);
-                                                              ok(node_index_results[0].nid, "nid");
+                                                              ok(node_index_results[0].nid, "nid");*/
                                                               
                                                               if (callback) {
                                                                 test_services_comment(callback);
                                                                 //callback();
                                                               }
-                                                            }
-                                                        });
-                                                    }); // Index
+                                                            //}
+                                                        //});
+                                                    //}); // Index
                                                     
                                                   }
                                               });
