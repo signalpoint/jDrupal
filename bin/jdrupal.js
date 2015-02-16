155fd3a221ea5ae971c55d000e23b0460dd601eb
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-02-15 22:42:32 -0500

    Update binaries.

--- bin/jdrupal.js
+++ bin/jdrupal.js
@@ -1017,6 +1017,57 @@ function entity_types() {
 }
 
 /**
+ * Assembles the data string used in Service calls.
+ * @param {String} entity_type
+ * @param {String} bundle
+ * @param {Object} entity
+ * @param {Object} options
+ * @return {String} data
+ */
+function entity_assemble_data(entity_type, bundle, entity, options) {
+  try {
+    // @TODO - this is an old function that was once deprecated, however the
+    // Services module appears to require a application/x-www-form-urlencoded
+    // content type when using PUT via the Comment Update resource, so we need
+    // this function to generate the URL encoded string.
+    var data = '';
+    for (var property in entity) {
+      if (entity.hasOwnProperty(property)) {
+        var type = typeof entity[property];
+        // Assemble field items.
+        if (type === 'object') {
+          for (var language in entity[property]) {
+            if (entity[property].hasOwnProperty(language)) {
+              for (var delta in entity[property][language]) {
+                if (entity[property][language].hasOwnProperty(delta)) {
+                  for (var value in entity[property][language][delta]) {
+                    if (
+                      entity[property][language][delta].hasOwnProperty(value)) {
+                      data += property +
+                        '[' + language + '][' + delta + '][' + value + ']=' +
+                        encodeURIComponent(
+                          entity[property][language][delta][value]
+                        ) + '&';
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        // Assemble flat properties.
+        else {
+          data += property + '=' + encodeURIComponent(entity[property]) + '&';
+        }
+      }
+    }
+    if (data != '') { data = data.substring(0, data.length - 1); }
+    return data;
+  }
+  catch (error) { console.log('entity_assemble_data - ' + error); }
+}
+
+/**
  * Loads a file, given a file id.
  * @param {Number} fid
  * @param {Object} options
@@ -1289,7 +1340,14 @@ Drupal.services.call = function(options) {
                 contentType = 'application/x-www-form-urlencoded';
               }
             }
-            else if (method == 'PUT') { contentType = 'application/json'; }
+            else if (method == 'PUT') {
+              contentType = 'application/json';
+              // The comment update resource needs a url encoded data string.
+              if (options.service == 'comment' &&
+                options.resource == 'update') {
+                contentType = 'application/x-www-form-urlencoded';
+              }
+            }
 
             // Anyone overriding the content type?
             if (options.contentType) { contentType = options.contentType; }
@@ -1693,6 +1751,13 @@ function entity_update(entity_type, bundle, entity, options) {
   try {
     var entity_wrapper = _entity_wrap(entity_type, entity);
     var primary_key = entity_primary_key(entity_type);
+    var data = null;
+    // The comment update resource needs the data url encoded for some reason,
+    // all others get stringified.
+    if (entity_type == 'comment') {
+      data = entity_assemble_data(entity_type, bundle, entity, options);
+    }
+    else { data = JSON.stringify(entity_wrapper); }
     Drupal.services.call({
         method: 'PUT',
         path: entity_type + '/' + entity[primary_key] + '.json',
@@ -1701,11 +1766,11 @@ function entity_update(entity_type, bundle, entity, options) {
         entity_type: entity_type,
         entity_id: entity[entity_primary_key(entity_type)],
         bundle: bundle,
-        data: JSON.stringify(entity_wrapper),
-        success: function(data) {
+        data: data,
+        success: function(result) {
           try {
             _entity_local_storage_delete(entity_type, entity[primary_key]);
-            if (options.success) { options.success(data); }
+            if (options.success) { options.success(result); }
           }
           catch (error) { console.log('entity_update - success - ' + error); }
         },
