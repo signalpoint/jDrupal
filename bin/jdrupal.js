1ac1e515a77eb7da3b37214f09bc5174d22923a4
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-07-22 14:07:40 -0500

    Update binary.

--- bin/jdrupal.js
+++ bin/jdrupal.js
@@ -1854,7 +1854,9 @@ function entity_index_build_query_string(query) {
           if (query.parameters_op.hasOwnProperty(parameter_op)) {
             var key = encodeURIComponent(parameter_op);
             var value = encodeURIComponent(query.parameters_op[parameter_op]);
+            // @TODO remove double compatability upon resolution of #2537968 on d.o
             parameters_op += 'parameters_op[' + key + ']=' + value + '&';
+            parameters_op += 'options[parameters_op][' + key + ']=' + value + '&';
           }
       }
       if (parameters_op != '') {
@@ -1868,7 +1870,9 @@ function entity_index_build_query_string(query) {
           if (!query.orderby.hasOwnProperty(column)) { continue; }
           var key = encodeURIComponent(column);
           var value = encodeURIComponent(query.orderby[column]);
+          // @TODO remove double compatability upon resolution of #2537968 on d.o
           orderby += 'orderby[' + key + ']=' + value + '&';
+          orderby += 'options[orderby][' + key + ']=' + value + '&';
       }
       if (orderby != '') {
         orderby = orderby.substring(0, orderby.length - 1);
