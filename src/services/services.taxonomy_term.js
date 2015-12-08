977706feb25435a7fa0ad1aabc5d36242839dc8f
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-12-08 08:33:20 -0500

    Remove more old crud, knyuck knyuck.

--- src/services/services.taxonomy_term.js
+++ src/services/services.taxonomy_term.js
@@ -1,56 +1,4 @@
 /**
- * Creates a taxonomy term.
- * @param {Object} taxonomy_term
- * @param {Object} options
- */
-function taxonomy_term_create(taxonomy_term, options) {
-  try {
-    services_resource_defaults(options, 'taxonomy_term', 'create');
-    entity_create('taxonomy_term', null, taxonomy_term, options);
-  }
-  catch (error) { console.log('taxonomy_term_create - ' + error); }
-}
-
-/**
- * Retrieves a taxonomy term.
- * @param {Number} ids
- * @param {Object} options
- */
-function taxonomy_term_retrieve(ids, options) {
-  try {
-    services_resource_defaults(options, 'taxonomy_term', 'retrieve');
-    entity_retrieve('taxonomy_term', ids, options);
-  }
-  catch (error) { console.log('taxonomy_term_retrieve - ' + error); }
-}
-
-/**
- * Update a taxonomy term.
- * @param {Object} taxonomy_term
- * @param {Object} options
- */
-function taxonomy_term_update(taxonomy_term, options) {
-  try {
-    services_resource_defaults(options, 'taxonomy_term', 'update');
-    entity_update('taxonomy_term', null, taxonomy_term, options);
-  }
-  catch (error) { console.log('taxonomy_term_update - ' + error); }
-}
-
-/**
- * Delete a taxonomy term.
- * @param {Number} tid
- * @param {Object} options
- */
-function taxonomy_term_delete(tid, options) {
-  try {
-    services_resource_defaults(options, 'taxonomy_term', 'delete');
-    entity_delete('taxonomy_term', tid, options);
-  }
-  catch (error) { console.log('taxonomy_term_delete - ' + error); }
-}
-
-/**
  * Perform a taxonomy_term index.
  * @param {Object} query
  * @param {Object} options
@@ -62,4 +10,3 @@ function taxonomy_term_index(query, options) {
   }
   catch (error) { console.log('taxonomy_term_index - ' + error); }
 }
-
--- src/services/services.taxonomy_vocabulary.js
+++ src/services/services.taxonomy_vocabulary.js
@@ -1,73 +1,4 @@
 /**
- * Creates a taxonomy vocabulary.
- * @param {Object} taxonomy_vocabulary
- * @param {Object} options
- */
-function taxonomy_vocabulary_create(taxonomy_vocabulary, options) {
-  try {
-    // Set a default machine name if one wasn't provided.
-    if (!taxonomy_vocabulary.machine_name && taxonomy_vocabulary.name) {
-      taxonomy_vocabulary.machine_name =
-        taxonomy_vocabulary.name.toLowerCase().replace(' ', '_');
-    }
-    services_resource_defaults(options, 'taxonomy_vocabulary', 'create');
-    entity_create('taxonomy_vocabulary', null, taxonomy_vocabulary, options);
-  }
-  catch (error) { console.log('taxonomy_vocabulary_create - ' + error); }
-}
-
-/**
- * Retrieves a comment.
- * @param {Number} ids
- * @param {Object} options
- */
-function taxonomy_vocabulary_retrieve(ids, options) {
-  try {
-    services_resource_defaults(options, 'taxonomy_vocabulary', 'retrieve');
-    entity_retrieve('taxonomy_vocabulary', ids, options);
-  }
-  catch (error) { console.log('taxonomy_vocabulary_retrieve - ' + error); }
-}
-
-/**
- * Update a taxonomy vocabulary.
- * @param {Object} taxonomy_vocabulary
- * @param {Object} options
- */
-function taxonomy_vocabulary_update(taxonomy_vocabulary, options) {
-  try {
-    // We need to make sure a machine_name was provided, otherwise it seems the
-    // Services module will update a vocabulary and clear out its machine_name
-    // if we don't provide it.
-    if (!taxonomy_vocabulary.machine_name ||
-      taxonomy_vocabulary.machine_name == '') {
-      var message = 'taxonomy_vocabulary_update - missing machine_name';
-      console.log(message);
-      if (options.error) {
-        options.error(null, 406, message);
-      }
-      return;
-    }
-    services_resource_defaults(options, 'taxonomy_vocabulary', 'update');
-    entity_update('taxonomy_vocabulary', null, taxonomy_vocabulary, options);
-  }
-  catch (error) { console.log('taxonomy_vocabulary_update - ' + error); }
-}
-
-/**
- * Delete a taxonomy vocabulary.
- * @param {Number} vid
- * @param {Object} options
- */
-function taxonomy_vocabulary_delete(vid, options) {
-  try {
-    services_resource_defaults(options, 'taxonomy_vocabulary', 'delete');
-    entity_delete('taxonomy_vocabulary', vid, options);
-  }
-  catch (error) { console.log('taxonomy_vocabulary_delete - ' + error); }
-}
-
-/**
  * Perform a taxonomy_vocabulary index.
  * @param {Object} query
  * @param {Object} options
@@ -79,4 +10,3 @@ function taxonomy_vocabulary_index(query, options) {
   }
   catch (error) { console.log('taxonomy_vocabulary_index - ' + error); }
 }
-
