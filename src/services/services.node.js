1cbd6c916116942e880793e88af69e95946b3d60
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-12-08 08:14:38 -0500

    Delete old node crud functions.

--- src/services/services.node.js
+++ src/services/services.node.js
@@ -1,56 +1,4 @@
 /**
- * Creates a node.
- * @param {Object} node
- * @param {Object} options
- */
-function node_create(node, options) {
-  try {
-    services_resource_defaults(options, 'node', 'create');
-    entity_create('node', node.getType(), node, options);
-  }
-  catch (error) { console.log('node_create - ' + error); }
-}
-
-/**
- * Retrieves a node.
- * @param {Number} ids
- * @param {Object} options
- */
-function node_retrieve(ids, options) {
-  try {
-    services_resource_defaults(options, 'node', 'retrieve');
-    entity_retrieve('node', ids, options);
-  }
-  catch (error) { console.log('node_retrieve - ' + error); }
-}
-
-/**
- * Update a node.
- * @param {Object} node
- * @param {Object} options
- */
-function node_update(node, options) {
-  try {
-    services_resource_defaults(options, 'node', 'update');
-    entity_update('node', node.getType(), node, options);
-  }
-  catch (error) { console.log('node_update - ' + error); }
-}
-
-/**
- * Delete a node.
- * @param {Number} nid
- * @param {Object} options
- */
-function node_delete(nid, options) {
-  try {
-    services_resource_defaults(options, 'node', 'delete');
-    entity_delete('node', nid, options);
-  }
-  catch (error) { console.log('node_delete - ' + error); }
-}
-
-/**
  * Perform a node index.
  * @param {Object} query
  * @param {Object} options
@@ -62,4 +10,3 @@ function node_index(query, options) {
   }
   catch (error) { console.log('node_index - ' + error); }
 }
-
