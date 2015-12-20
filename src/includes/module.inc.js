e115a1fbf085c32da8517f2bf42a941527de1a5f
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-12-20 14:42:44 -0500

    Change module invocation into promise and promise alls.

--- src/includes/module.inc.js
+++ src/includes/module.inc.js
@@ -51,34 +51,18 @@ jDrupal.moduleImplements = function(hook) {
  * @return {*}
  */
 jDrupal.moduleInvoke = function(module, hook) {
-  try {
-    var module_invocation_results = null;
-    if (jDrupal.moduleLoad(module)) {
-      var module_arguments = Array.prototype.slice.call(arguments);
-      var function_name = module + '_' + hook;
-      if (jDrupal.functionExists(function_name)) {
-        // Get the hook function.
-        var fn = window[function_name];
-        // Remove the module name and hook from the arguments.
-        module_arguments.splice(0, 2);
-        // If there are no arguments, just call the hook directly, otherwise
-        // call the hook and pass along all the arguments.
-        if (Object.getOwnPropertyNames(module_arguments).length == 0) {
-          module_invocation_results = fn();
-        }
-        else { module_invocation_results = fn.apply(null, module_arguments); }
-      }
-    }
-    else {
-      var msg = 'jDrupal.moduleInvoke - failed to load: ' + module;
-      console.log(msg);
-    }
-    return module_invocation_results;
-  }
-  catch (error) { console.log('jDrupal.moduleInvoke - ' + error); }
+  if (!jDrupal.moduleLoad(module)) { return; }
+  var name = module + '_' + hook;
+  if (!jDrupal.functionExists(name)) { return; }
+  // Get the hook function, then remove the module name and hook from the
+  // arguments. Then if there are no arguments, just call the hook directly,
+  // otherwise call the hook and pass along all the arguments.
+  var fn = window[name];
+  var module_arguments = Array.prototype.slice.call(arguments);
+  module_arguments.splice(0, 2);
+  if (Object.getOwnPropertyNames(module_arguments).length == 0) { return fn(); }
+  else { return fn.apply(null, module_arguments); }
 };
-
-jDrupal._moduleInvokeResults = null;
 
 /**
  * Given a hook name, this will invoke all modules that implement the hook.
@@ -86,48 +70,39 @@ jDrupal._moduleInvokeResults = null;
  * @return {Array}
  */
 jDrupal.moduleInvokeAll = function(hook) {
-  try {
+  var promises = [];
 
-    // Prepare the invocation results.
-    jDrupal._moduleInvokeResults = [];
+  // Copy the arguments and remove the hook name from the first index so the
+  // rest can be passed along to the hook.
+  var module_arguments = Array.prototype.slice.call(arguments);
+  module_arguments.splice(0, 1);
 
-    // Copy the arguments and remove the hook name from the first index so the
-    // rest can be passed along to the hook.
-    var module_arguments = Array.prototype.slice.call(arguments);
-    module_arguments.splice(0, 1);
-
-    // Figure out which modules are implementing this hook.
-    var modules = [];
-    for (var module in jDrupal.modules) {
-      if (!jDrupal.modules.hasOwnProperty(module)) { continue; }
-      if (!jDrupal.functionExists(module + '_' + hook)) { continue; }
-      modules.push(module);
-    }
-    if (jDrupal.isEmpty(modules)) { return; }
-
-    for (var i = 0; i < modules.length; i++) {
-      // If there are no arguments, just call the hook directly,
-      // otherwise call the hook and pass along all the arguments.
-      var invocation_results = null;
-      if (module_arguments.length == 0) {
-        invocation_results = jDrupal.moduleInvoke(module, hook);
-      }
-      else {
-        // Place the module name and hook name on the front of the
-        // arguments.
-        module_arguments.unshift(module, hook);
-        var fn = window['jDrupal'].moduleInvoke;
-        invocation_results = fn.apply(null, module_arguments);
-        module_arguments.splice(0, 2);
-      }
-      if (typeof invocation_results !== 'undefined') {
-        jDrupal._moduleInvokeResults.push(invocation_results);
-      }
-    }
-
-    return jDrupal._moduleInvokeResults;
+  // Figure out which modules are implementing this hook.
+  var modules = [];
+  for (var module in jDrupal.modules) {
+    if (!jDrupal.modules.hasOwnProperty(module)) { continue; }
+    if (!jDrupal.functionExists(module + '_' + hook)) { continue; }
+    modules.push(module);
   }
-  catch (error) { console.log('jDrupal.moduleInvokeAll - ' + error); }
+  if (jDrupal.isEmpty(modules)) { return; }
+
+  for (var i = 0; i < modules.length; i++) {
+    // If there are no arguments, just call the hook directly,
+    // otherwise call the hook and pass along all the arguments.
+    var invocation_results = null;
+    if (module_arguments.length == 0) {
+      promises.push(jDrupal.moduleInvoke(modules[i], hook));
+    }
+    else {
+      // Place the module name and hook name on the front of the
+      // arguments.
+      module_arguments.unshift(modules[i], hook);
+      promises.push(jDrupal.moduleInvoke.apply(null, module_arguments));
+      module_arguments.splice(0, 2);
+    }
+  }
+
+  return Promise.all(promises);
 };
 
 /**
