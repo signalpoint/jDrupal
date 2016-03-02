d92df66690f1041293dceaa340498fff15322288
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-03-01 20:13:23 -0500

    Clean up code comments.

--- src/core.js
+++ src/core.js
@@ -468,14 +468,11 @@ function time() {
  * @return {String}
  */
 function ucfirst(str) {
-  // http://kevin.vanzonneveld.net
-  // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +   bugfixed by: Onno Marsman
-  // +   improved by: Brett Zamir (http://brett-zamir.me)
-  // *     example 1: ucfirst('kevin van zonneveld');
-  // *     returns 1: 'Kevin van zonneveld'
+  // @see http://kevin.vanzonneveld.net
+  // + original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // + bugfixed by: Onno Marsman
+  // + improved by: Brett Zamir (http://brett-zamir.me)
   str += '';
   var f = str.charAt(0).toUpperCase();
   return f + str.substr(1);
 }
-
